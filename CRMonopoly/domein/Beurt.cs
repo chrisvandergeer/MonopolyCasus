4ebf494814e4ee8b07e78d5d68b3b98c875d750e
Roel de Weerd <roel.de.weerd@gmail.com>
2013-04-08 22:17:40 +0200

    Removed debug line

--- CRMonopoly/domein/Beurt.cs
+++ CRMonopoly/domein/Beurt.cs
@@ -28,7 +28,6 @@ namespace CRMonopoly.domein
         public string GooiDobbelstenen()
         {
             Worp worp = Worp.GooiDobbelstenen();
-            Debug.WriteLine(String.Format("Er is {0} gegooid.", worp.Gedobbeldeworp1 + worp.Gedobbeldeworp2));
             Gebeurtenis gebeurtenis = Speler.Verplaats(worp);
             gebeurtenis.VoerUit(Speler);
             return string.Format("{0} gooit {1} en {2}", Speler.Name, worp, gebeurtenis.ToString());
