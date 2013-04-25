e8d81006c56b364c8651dffca7e00ece0d30da68
Roel de Weerd <roel.de.weerd@gmail.com>
2013-04-25 21:38:59 +0200

    Added an extra test, but not yet finished. Added hyperlinks to Program

--- CRMonopoly/Program.cs
+++ CRMonopoly/Program.cs
@@ -5,6 +5,16 @@ using System.Text;
 using CRMonopoly.domein;
 using CRMonopoly.domein.gebeurtenis;
 
+// Usefull links: 
+// Mocking framework Fakes
+// http://www.richonsoftware.com/post/2012/04/05/Using-Stubs-and-Shim-to-Test-with-Microsoft-Fakes-in-Visual-Studio-11.aspx
+// Mocking framework Moles (voorloper van Fakes)
+//  http://research.microsoft.com/en-us/projects/moles/
+// Adding Unity to Your Application
+// http://msdn.microsoft.com/en-us/library/ff660927%28v=PandP.20%29.aspx
+
+
+
 namespace CRMonopoly
 {
     class Program
