07ed5bee7d999888f889fadf71e0bc20ec3f6b93
Chris <chrisvandergeer@gmail.com>
2013-06-03 23:17:22 +0200

    OntvangGeldVanIedereSpeler.cs geimplementeerd

---
+++ CRMonopoly/domein/gebeurtenis/OntvangGeldVanIedereSpeler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRMonopoly.domein.gebeurtenis
+{
+    class OntvangGeldVanIedereSpeler : AbstractGebeurtenis
+    {
+        private Monopolyspel _spel;
+        private int _bedrag;
+
+        public OntvangGeldVanIedereSpeler(Monopolyspel spel, int bedrag) : base("Ontvang " + bedrag + " van iedere speler")
+        {
+            _spel = spel;
+            _bedrag = bedrag;
+        }
+
+        public override GebeurtenisResult VoerUit(Speler speler)
+        {
+            _spel.Spelers.FindAll(s => !s.Equals(speler)).ForEach(s => s.Betaal(_bedrag, speler));
+            return GebeurtenisResult.Uitgevoerd(speler, "heeft van iedere speler", _bedrag, "ontvangen");
+        }
+
+        public override bool IsVerplicht()
+        {
+            return true;
+        }
+    }
+}
